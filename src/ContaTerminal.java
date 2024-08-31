d16557a9177a508552399939f0e3959d521c4594
alinecintracamargo <alineandradecintra@gmail.com>
2024-08-31 09:44:24 -0300

    conta criada

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // Instanciar o Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // Exibir as mensagens para o usuário
+        System.out.println("Por favor, digite o número da conta:");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da agência:");
+        String numeroAgencia = scanner.next();
+
+        System.out.println("Por favor, digite o nome do cliente:");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o saldo inicial:");
+        scanner.nextLine();
+        double saldo = scanner.nextDouble();
+
+        // Exibir a mensagem final da conta criada
+        System.out.println("Olá " + nomeCliente + ", obrigado(a) por criar uma conta em nosso banco. Sua agência é " 
+            + numeroAgencia + ", conta " + numeroConta + " e seu saldo é " + saldo + ", já disponível para saque.");
+
+        // Fechar o Scanner
+        scanner.close();
     }
 }
